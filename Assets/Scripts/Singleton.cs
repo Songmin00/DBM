25824d44562c4e9cdfa595e2ed3469e386f78b89
Songmin00 <dn7272@gmail.com>
2026-01-14 19:20:03 +0900

    [Feat] Singleton
    범용 싱글톤 클래스 추가.

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance => _instance;
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(this);
+        }
+    }
+}
