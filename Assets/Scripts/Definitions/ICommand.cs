5041e0ae51d0d035491ac1a4e26ef57f1a84b0c0
Songmin00 <dn7272@gmail.com>
2026-01-23 00:43:28 +0900

    [Feat] ICommand
    캐릭터 행동 인터페이스 ICommand 및 공용 행동인 MoveCommand, LookCommand 구현.

--- Assets/Scripts/Definitions/ICommand.cs
+++ Assets/Scripts/Definitions/ICommand.cs
@@ -1,4 +1,43 @@
+using UnityEngine;
+
+//모든 커맨드는 이걸 상속
 public interface ICommand
 {
     void Execute();
+}
+
+
+//공용 이동 & 방향전환 커맨드 정의
+public class MoveCommand : ICommand
+{
+    CharacterController _controller;
+    Vector2 _input;
+
+    public MoveCommand(CharacterController controller, Vector2 input)
+    {
+        _controller = controller;
+        _input = input;
+    }
+
+    public void Execute()
+    {
+        _controller.Move(_input);
+    }
+}
+
+public class LookCommand : ICommand
+{
+    CharacterController _controller;
+    Vector2 _input;
+
+    public LookCommand(CharacterController controller, Vector2 input)
+    {
+        _controller = controller;
+        _input = input;
+    }
+
+    public void Execute()
+    {
+        _controller.Look(_input);
+    }
 }
