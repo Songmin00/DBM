b075ca5412bc823b892add4fbb42b2203222d19b
Songmin00 <dn7272@gmail.com>
2026-01-23 00:46:29 +0900

    [Feat] InputHandler
    사용자 입력을 의도로 변환하기 위한 InputHandler 뼈대 구현.

---
+++ Assets/Scripts/InGameScene/InputHandler.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using Photon.Pun;
+
+public class InputHandler : MonoBehaviourPunCallbacks
+{
+    List<ICommand> _commands = new List<ICommand>();
+
+    public void OnMove()
+    {
+
+    }
+
+    public void OnLook()
+    {
+
+    }
+
+    public void OnLeftMouseClick()
+    {
+
+    }
+
+    public void OnLeftMouseHold()
+    {
+
+    }
+
+    public void OnRightMouseClick()
+    {
+
+    }
+
+    public void OnRightMouseHold()
+    {
+
+    }
+
+    public void OnCtrl()
+    {
+
+    }
+
+    public void OnSpace()
+    {
+
+    }
+
+    public void OnR()
+    {
+
+    }
+}
