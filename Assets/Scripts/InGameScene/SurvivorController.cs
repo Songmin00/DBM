e759f6856cf874561c47fb4a7140d4f1c774155e
Songmin00 <dn7272@gmail.com>
2026-01-22 16:25:20 +0900

    [Feat] SurvivorController
    생존자 공통 액션을 정의한 SurvivorController 클래스 뼈대 구현.

---
+++ Assets/Scripts/InGameScene/SurvivorController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+//생존자 공용 액션을 정의. 경우에 따라 발전기 수리와 판자 내리기 등은 인터페이스를 통해 Interact()로 통합시킬 것.
+public class SurvivorController : CharacterController
+{
+    public void Run() //달리기
+    {
+
+    }
+    
+    public void Sit() //앉기
+    {
+
+    }
+
+    public void FixGenerator() //발전기 수리
+    {
+
+    }
+
+    public void DownPanel() //판자 내리기
+    {
+
+    }
+
+    public void Heal() //생존자 치료
+    {
+
+    }
+
+    public void Resque() //생존자 구출
+    {
+
+    }
+
+    public void SkillCheck() //미니게임 스킬 체크
+    {
+
+    }
+
+}
