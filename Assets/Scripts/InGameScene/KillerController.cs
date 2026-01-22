85975759cc2debc86efed32d119c566d21204103
Songmin00 <dn7272@gmail.com>
2026-01-22 15:53:27 +0900

    [Feat] KillerController
    킬러 공용 액션을 담당할 KillerController 뼈대 구현.

---
+++ Assets/Scripts/InGameScene/KillerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+//킬러가 가지고 있는 공통 액션만 정의. 각각 특수능력은 KillerAbility로.
+public class KillerController : CharacterController
+{
+    public void Attack() //단거리 공격
+    {
+
+    }
+
+    public void Lunge() //대쉬 공격
+    {
+
+    }
+
+    public void KickPanel() //판자 부수기
+    {
+
+    }
+
+    public void KickGenerator() //발전기 부수기
+    {
+
+    }
+
+    public void Lift() //쓰러진 생존자 들기
+    {
+
+    }
+
+    public void Drop() //들고 있는 생존자 내려놓기
+    {
+
+    }
+
+    
+}
