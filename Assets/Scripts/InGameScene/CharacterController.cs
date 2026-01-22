ee36ed206d7c156ae0c9ae756075773e3fad760b
Songmin00 <dn7272@gmail.com>
2026-01-22 15:52:55 +0900

    [Fix] CharacterController
    테스트용 공용 이동속도값 및 주석문 추가.
    창틀 뛰어넘기용 Vault 매서드 추가.

--- Assets/Scripts/InGameScene/CharacterController.cs
+++ Assets/Scripts/InGameScene/CharacterController.cs
@@ -5,7 +5,7 @@ public class CharacterController : MonoBehaviour
     protected GameObject _character;
     protected Rigidbody _rb;
 
-    protected float _moveSpeed;
+    protected float _moveSpeed = 4;
     private void Awake()
     {
         _rb = _character.GetComponent<Rigidbody>();
@@ -16,7 +16,7 @@ public class CharacterController : MonoBehaviour
         _character = character;
     }
 
-    public virtual void Move(Vector2 input)
+    public virtual void Move(Vector2 input) //공용 이동 로직
     {
         Vector3 dir = new Vector3(input.x, 0f, input.y);
         Vector3 velocity = dir.normalized * _moveSpeed;
@@ -24,11 +24,16 @@ public class CharacterController : MonoBehaviour
         _rb.linearVelocity = new Vector3(velocity.x, 0f, velocity.z);
     }
 
-    public virtual void Look(Vector2 direction)
+    public virtual void Look(Vector2 direction) //공용 시점 이동 로직
     {
         Vector3 dir = new Vector3(direction.x, 0f, direction.y);
         Quaternion rot = Quaternion.LookRotation(dir);
 
         _rb.MoveRotation(rot);
+    }
+
+    public virtual void Vault() //공용 창틀 뛰어넘기 로직
+    {
+
     }
 }
