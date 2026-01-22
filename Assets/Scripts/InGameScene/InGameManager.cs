0b782084a6d6dcf8fc43a91efc68734fc9ae8592
Songmin00 <dn7272@gmail.com>
2026-01-23 01:17:28 +0900

    [Fix] InGameManager
    InGameManager의 테스트용 매서드 제거.
    캐릭터 프리팹을 PlayerStateManager로부터 받아오도록 수정.

--- Assets/Scripts/InGameScene/InGameManager.cs
+++ Assets/Scripts/InGameScene/InGameManager.cs
@@ -7,13 +7,15 @@ public class InGameManager : MonoBehaviourPunCallbacks
 {
     public static InGameManager Instance;
 
-    [SerializeField] GameObject _playerPrefab;
+    GameObject _playerPrefab;
     Vector3 _spawnPos = new Vector3(0, 1.5f, 0);
 
 
     private void Start() //씬이 너무 빨리 불러와져서 start가 룸 들어가기 전에 호출되는 문제가 있음
     {
         Instance = this;
+
+        _playerPrefab = CharacterStateManager.Instance.CharacterPrefab;
 
         //if (PlayerManager.LocalPlayerInstance == null)
         {
@@ -24,7 +26,7 @@ public class InGameManager : MonoBehaviourPunCallbacks
     private IEnumerator SpawnPlayerWhenConnected()
     {
         yield return new WaitUntil(() => PhotonNetwork.InRoom);
-        //PlayerManager.LocalPlayerInstance = PhotonNetwork.Instantiate(_playerPrefab.name, _spawnPos, Quaternion.identity);
+        PhotonNetwork.Instantiate(_playerPrefab.name, _spawnPos, Quaternion.identity);
     }
 
     public override void OnLeftRoom()
