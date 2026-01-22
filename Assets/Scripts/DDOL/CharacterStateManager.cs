e40596ef93853f0582401c0c15aa8632cd1821ed
Songmin00 <dn7272@gmail.com>
2026-01-22 15:30:23 +0900

    [Fix] CharacterController
    필드를 프로퍼티화하여 외부 조회가 가능하도록 수정.

--- Assets/Scripts/DDOL/CharacterStateManager.cs
+++ Assets/Scripts/DDOL/CharacterStateManager.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 //로비에서 고른 캐릭터 정보를 인게임으로 가져가기 위한 클래스
 public class CharacterStateManager : Singleton<CharacterStateManager>
 {
-    PlayerType _playerType; //킬러인지 생존자인지 구분
-    GameObject _characterPrefab;
+    public PlayerType PlayerType { get; private set; } //킬러인지 생존자인지 구분
+    public GameObject CharacterPrefab { get; private set; }
 
     public void SetPlayerType(PlayerType type) //인풋 설정을 위한 타입 보관
     {
-        _playerType = type;
+        PlayerType = type;
     }
 
     public void SetCharacterPrefab(GameObject prefab) //캐릭터 생성을 위한 프리팹 보관
     {
-        _characterPrefab = prefab;
+        CharacterPrefab = prefab;
     }
 }
