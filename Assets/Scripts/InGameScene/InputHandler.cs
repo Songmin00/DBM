9c5700cf28b18e8eea4f2a92775b12cb0b7e3d6e
Songmin00 <dn7272@gmail.com>
2026-01-26 02:18:37 +0900

    [Fix] InputHandler
    인풋핸들러가 하위 리졸버 클래스를 생성하여 액션을 분류하도록 변경. 이동 입력 처리 구현.

--- Assets/Scripts/InGameScene/InputHandler.cs
+++ Assets/Scripts/InGameScene/InputHandler.cs
@@ -1,18 +1,49 @@
 using System.Collections.Generic;
+using System.Collections;
 using Photon.Pun;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviourPunCallbacks
 {
-    List<ICommand> _commands = new List<ICommand>();
+    private InputTypeResolver _inputTypeResolver;
+    private PlayerType _inputType;
 
-    public void OnMove()
+    List<ICommand> _commands = new List<ICommand>(); //매 프레임 실행할 커맨드 관리용 리스트
+    
+    
+
+    private void Start()
     {
-
+        _inputType = CharacterStateManager.Instance.PlayerType;
+        StartCoroutine(SetTypeRoutine()); // InGameManager가 캐릭턱를 생성하기를 기다렸다가 준비되면 정보 받아와서 하위 시스템들 세팅.
     }
 
-    public void OnLook()
+    private IEnumerator SetTypeRoutine()
     {
+        yield return new WaitUntil(() => InGameManager.Instance != null && InGameManager.Instance.isReady);
+        _inputTypeResolver = new InputTypeResolver(CharacterStateManager.Instance.PlayerType);
+    }
 
+    private void Update()
+    {        
+        foreach (var command in _commands)
+        {
+            command.Execute();
+        }
+        _commands.Clear();
+    }
+
+    public void OnWASD(InputAction.CallbackContext ctx)
+    {
+        Vector2 input = ctx.ReadValue<Vector2>();
+        _commands.Add(_inputTypeResolver.OnWASD(input));
+    }
+
+    public void OnMousePointer(InputAction.CallbackContext ctx)
+    {        
+        Vector2 input = ctx.ReadValue<Vector2>();
+        _commands.Add(_inputTypeResolver.OnMousePointer(input));        
     }
 
     public void OnLeftMouseClick()
@@ -46,6 +77,11 @@ public class InputHandler : MonoBehaviourPunCallbacks
     }
 
     public void OnR()
+    {
+
+    }
+
+    public void OnShift()
     {
 
     }
