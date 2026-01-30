88ff07549a24e7ccb0981ee9671e2ac5287a43dc
Songmin00 <dn7272@gmail.com>
2026-01-31 03:10:52 +0900

    [Fix] SurvivorController
    이동 로직 개선.
    Interact 매서드 구현.

--- Assets/Scripts/InGameScene/SurvivorController.cs
+++ Assets/Scripts/InGameScene/SurvivorController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class SurvivorController : CharacterControllerBase
 {
     [SerializeField] Transform _cameraAnchor;
+    [SerializeField] SurvivorInteractManager _interactManager;
+    bool _isInteracting = false;
 
     protected override void Awake()
     {
@@ -15,15 +17,18 @@ public class SurvivorController : CharacterControllerBase
         }
     }
 
+    protected override void FixedUpdate()
+    {
+        if (_isInteracting)
+        {
+            return;
+        }
+        MoveRogic();
+    }
+
     public Transform GetCameraAnchor()
     {
         return _cameraAnchor;
-    }
-
-    public override void Move(Vector2 input) //공용 이동 로직
-    {
-        base.Move(input);
-        Look(input);
     }
 
     public void Run() //달리기
@@ -40,6 +45,22 @@ public class SurvivorController : CharacterControllerBase
     {
 
     }
+
+    public void Interact(bool interact)
+    {
+        if (interact)
+        {
+            _interactManager.StartInteract();
+            _isInteracting = true;
+            Debug.Log("인터랙션 시작!");
+        }
+        else
+        {
+            _interactManager.StopInteract();
+            _isInteracting = false;
+            Debug.Log("인터랙션 종료!");
+        }        
+    }    
 
     public void SkillCheck() //미니게임 스킬 체크
     {
@@ -67,4 +88,37 @@ public class SurvivorController : CharacterControllerBase
 
     }
 
+    protected override void MoveRogic()
+    {
+        
+        if (MoveInput == Vector2.zero)
+        {
+            _rb.linearVelocity = new Vector3(0, _rb.linearVelocity.y, 0);
+            return;
+        }
+
+        Transform cam = Camera.main.transform;
+
+        
+        Vector3 camForward = cam.forward;
+        camForward.y = 0f;
+        camForward.Normalize();
+
+        Vector3 camRight = cam.right;
+        camRight.y = 0f;
+        camRight.Normalize();
+
+        Vector3 dir = (camForward * MoveInput.y + camRight * MoveInput.x).normalized;
+        Vector3 velocity = dir * _moveSpeed;
+
+        
+        _rb.linearVelocity = new Vector3(velocity.x, _rb.linearVelocity.y, velocity.z);
+
+        
+        if (dir != Vector3.zero)
+        {        
+            Quaternion targetRot = Quaternion.LookRotation(dir);
+            _rb.MoveRotation(Quaternion.Slerp(_rb.rotation, targetRot, 15f * Time.fixedDeltaTime));
+        }
+    }
 }
