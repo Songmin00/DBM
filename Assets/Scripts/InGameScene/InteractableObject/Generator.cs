ad57a8ed5f83024f9ac219affdf9b78cfd6b22b4
Songmin00 <dn7272@gmail.com>
2026-01-31 03:25:46 +0900

    [Fix] Generator
    네트워크 동기화를 위한 RPC 추가 구현.

--- Assets/Scripts/InGameScene/InteractableObject/Generator.cs
+++ Assets/Scripts/InGameScene/InteractableObject/Generator.cs
@@ -1,39 +1,50 @@
+using Photon.Pun;
 using UnityEngine;
 
-public class Generator : MonoBehaviour, IInteractable
+public class Generator : MonoBehaviourPun, IInteractable
 {
-    private float gauge = 80;
-    private float _currentGauge = 0;    
+    private float gauge = 80f;
+    private float _currentGauge = 0f;
     private int _fixingSurvivors = 0;
-    private bool  _isFixed = false;
+    private bool _isFixed = false;
 
     private void Update()
     {
-        if (_isFixed)
-        {
-            return;
-        }
-        if (_fixingSurvivors <= 0)
-        {
-            return;
-        }
+        if (!PhotonNetwork.IsMasterClient) return;
+        if (_isFixed) return;
+        if (_fixingSurvivors <= 0) return;
 
-        FixingRogic();
+        FixingLogic();
     }
 
     public void StartInteract()
-    {        
-        _fixingSurvivors++;
-    }
-    public void StopInteract()
-    {        
-        _fixingSurvivors = Mathf.Max(0, _fixingSurvivors - 1);             
+    {
+        photonView.RPC(nameof(RPC_AddFixer), RpcTarget.MasterClient);
     }
 
-    private void FixingRogic()
+    public void StopInteract()
     {
-        _currentGauge += (1 * Time.deltaTime) * (1 + ((_fixingSurvivors - 1) * 0.5f));
-        Debug.Log("ÁøÇàµµ"+_currentGauge);
+        photonView.RPC(nameof(RPC_RemoveFixer), RpcTarget.MasterClient);
+    }
+
+    [PunRPC]
+    private void RPC_AddFixer()
+    {
+        _fixingSurvivors++;
+    }
+
+    [PunRPC]
+    private void RPC_RemoveFixer()
+    {
+        _fixingSurvivors = Mathf.Max(0, _fixingSurvivors - 1);
+    }
+
+    private void FixingLogic()
+    {
+        _currentGauge += Time.deltaTime * (1 + (_fixingSurvivors - 1) * 0.5f);
+
+        photonView.RPC(nameof(RPC_SyncGauge), RpcTarget.Others, _currentGauge);
+
         if (_currentGauge >= gauge)
         {
             _currentGauge = gauge;
@@ -41,7 +52,20 @@ public class Generator : MonoBehaviour, IInteractable
         }
     }
 
+    [PunRPC]
+    private void RPC_SyncGauge(float value)
+    {
+        _currentGauge = value;
+    }
+
     private void GetFixed()
+    {
+        _isFixed = true;
+        photonView.RPC(nameof(RPC_Fixed), RpcTarget.All);
+    }
+
+    [PunRPC]
+    private void RPC_Fixed()
     {
         _isFixed = true;
         Debug.Log("Fixed!!");
