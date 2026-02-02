ca7ab2888f09bd6498466cd2be1f0f0086726335
Songmin00 <dn7272@gmail.com>
2026-02-02 20:44:08 +0900

    [Fix] ICommands
    커맨드 스크립트 통합.

--- Assets/Scripts/Definitions/ICommand.cs
+++ Assets/Scripts/Definitions/ICommand.cs
@@ -10,10 +10,9 @@ public class NullCommand : ICommand
 {
     public void Execute()
     {
-        
+
     }
 }
-
 
 //공용 이동 & 방향전환 커맨드 정의
 public class MoveCommand : ICommand
@@ -28,7 +27,7 @@ public class MoveCommand : ICommand
     }
 
     public void Execute()
-    {        
+    {
         _controller.Move(_input);
     }
 }
@@ -49,13 +48,14 @@ public class LookCommand : ICommand
         _controller.Look(_input);
     }
 }
+
 public class VaultCommand : ICommand
 {
-    CharacterControllerBase _controller;    
+    CharacterControllerBase _controller;
 
     public VaultCommand(CharacterControllerBase controller)
     {
-        _controller = controller;        
+        _controller = controller;
     }
 
     public void Execute()
@@ -64,3 +64,241 @@ public class VaultCommand : ICommand
     }
 }
 
+public class AttackCommand : ICommand //단거리 공격
+{
+    KillerController _controller;
+
+    public AttackCommand(KillerController characterController)
+    {
+        _controller = characterController;
+    }
+
+    public void Execute()
+    {
+        _controller.Attack();
+    }
+}
+
+public class LungeAttackCommand : ICommand //대쉬 공격
+{
+    KillerController _controller;
+
+    public LungeAttackCommand(KillerController characterController)
+    {
+        _controller = characterController;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class KickPanelCommand : ICommand //판자 부수기
+{
+    KillerController _controller;
+
+    public KickPanelCommand(KillerController characterController)
+    {
+        _controller = characterController;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class KickGeneratorCommand : ICommand //발전기 부수기
+{
+    KillerController _controller;
+
+    public KickGeneratorCommand(KillerController characterController)
+    {
+        _controller = characterController;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class KillerInteractCommand : ICommand //쓰러진 생존자 들기
+{
+    KillerController _controller;
+
+    public KillerInteractCommand(KillerController characterController)
+    {
+        _controller = characterController;
+    }
+
+    public void Execute()
+    {
+        _controller.Interact();
+    }
+}
+
+public class DropCommand : ICommand //들고 있는 생존자 내려놓기
+{
+    KillerController _controller;
+
+    public DropCommand(KillerController characterController)
+    {
+        _controller = characterController;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class CatchCommand : ICommand
+{
+    KillerController _controller;
+
+    public CatchCommand(KillerController characterController)
+    {
+        _controller = characterController;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class RunCommand : ICommand //달리기
+{
+    SurvivorController _survivorController;
+    bool _isRun;
+    public RunCommand(SurvivorController controller, bool isHold)
+    {
+        _survivorController = controller;
+        _isRun = isHold;
+    }
+
+    public void Execute()
+    {
+        _survivorController.Run(_isRun);
+    }
+}
+
+public class SitCommand : ICommand //앉기
+{
+    SurvivorController _survivorController;
+    bool _isSit;
+
+    public SitCommand(SurvivorController controller, bool isSit)
+    {
+        _survivorController = controller;
+        _isSit = isSit;
+    }
+
+    public void Execute()
+    {
+        _survivorController.Sit(_isSit);
+    }
+}
+
+public class DownPanelCommand : ICommand //판자 내리기
+{
+    SurvivorController _survivorController;
+    public DownPanelCommand(SurvivorController controller)
+    {
+        _survivorController = controller;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class FixGeneratorCommand : ICommand //수리
+{
+    SurvivorController _survivorController;
+    public FixGeneratorCommand(SurvivorController controller)
+    {
+        _survivorController = controller;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class HealCommand : ICommand //치료
+{
+    SurvivorController _survivorController;
+    public HealCommand(SurvivorController controller)
+    {
+        _survivorController = controller;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class ResquerCommand : ICommand //구출
+{
+    SurvivorController _survivorController;
+    public ResquerCommand(SurvivorController controller)
+    {
+        _survivorController = controller;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class SkillCheckCommand : ICommand //미니게임 스킬 체크
+{
+    SurvivorController _survivorController;
+    public SkillCheckCommand(SurvivorController controller)
+    {
+        _survivorController = controller;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class UseItemCommand : ICommand //아이템 사용
+{
+    SurvivorController _survivorController;
+    public UseItemCommand(SurvivorController controller)
+    {
+        _survivorController = controller;
+    }
+
+    public void Execute()
+    {
+
+    }
+}
+
+public class SurvivorInteractCommand : ICommand
+{
+    SurvivorController _survivorController;
+    bool _interact;
+
+    public SurvivorInteractCommand(SurvivorController controller, bool interact)
+    {
+        _survivorController = controller;
+        _interact = interact;
+    }
+
+    public void Execute()
+    {
+        _survivorController.Interact(_interact);
+    }
+}
